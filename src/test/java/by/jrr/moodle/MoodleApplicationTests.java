507b8511e307f21e16e0e072d56c0c28c9c73f81
max-java <6666350@gmail.com>
2020-03-29 01:12:25 +0300

    add spring-boot empty project

---
+++ src/test/java/by/jrr/moodle/MoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.moodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
