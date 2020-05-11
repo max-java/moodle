98644bbaf77c378065a23b79df2703fb054ee2db
max-java <6666350@gmail.com>
2020-05-11 04:21:10 +0300

    Create constants to hold uris and views locations

---
+++ src/main/java/by/jrr/constant/Endpoint.java
@@ -0,0 +1,7 @@
+package by.jrr.constant;
+
+public class Endpoint {
+    public static final String HOME = "/";
+    public static final String TOPIC = "/topic";
+    public static final String TOPIC_LIST = "/topics";
+}
---
+++ src/main/java/by/jrr/constant/View.java
@@ -0,0 +1,7 @@
+package by.jrr.constant;
+
+public class View {
+    public static final String TOPIC = "topic";
+    public static final String TOPIC_LIST = "topicList";
+    public static final String PAGE_404 = "error";
+}
