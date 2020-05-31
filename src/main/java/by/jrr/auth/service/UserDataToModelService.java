3397aecdd19c831be9f9bf24ac16868d6bb073ee
max-java <6666350@gmail.com>
2020-06-01 02:45:35 +0300

    add link to current user profile

--- src/main/java/by/jrr/auth/service/UserDataToModelService.java
+++ src/main/java/by/jrr/auth/service/UserDataToModelService.java
@@ -2,12 +2,11 @@ package by.jrr.auth.service;
 
 import by.jrr.auth.bean.User;
 import by.jrr.auth.bean.UserData;
+import by.jrr.profile.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 
 @Service
@@ -17,29 +16,32 @@ public class UserDataToModelService {
     UserService userService;
     @Autowired
     UserAccessService userAccessService;
+    @Autowired
+    ProfileService profileService; // TODO: 01/06/20 this doesn't belong here. set current user profile otherwise
 
     public ModelAndView setData(ModelAndView mov) { // TODO: 28/05/20 replace by different methods simmilar to getInstance with zero parameters and cache it
         setIsAuthenticated(mov);
-        setUserNameAndLastName(mov);
+        setCurrentUserProfile(mov);
         setListOfUsers(mov);
         return mov;
     }
 
-    public ModelAndView setIsAuthenticated(ModelAndView modelAndView) {
+    private ModelAndView setIsAuthenticated(ModelAndView modelAndView) {
         return modelAndView.addObject(UserData.IS_AUTHENTICATED.name(), userAccessService.isCurrentUserAuthenticated());
     }
 
     /** set authenticated user data */
-    public ModelAndView setUserNameAndLastName(ModelAndView modelAndView) {
+    private ModelAndView setCurrentUserProfile(ModelAndView modelAndView) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByUserName(auth.getName());
         if (user != null) {
             modelAndView.addObject(UserData.USER_NAME_AND_LASTNAME.name(), user.getName() + " " + user.getLastName());
+            modelAndView.addObject(UserData.CURRENT_PROFILE.name(), profileService.getCurrentUserProfile());
         }
         return modelAndView;
     }
 
-    public ModelAndView setListOfUsers(ModelAndView mov) {
+    private ModelAndView setListOfUsers(ModelAndView mov) {
         return mov.addObject(UserData.USER_LIST.name(), userService.findAllUsers());
     }
 }
