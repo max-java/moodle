9c828d775f477bed14c6037162b47d2abfcd9b45
max-java <6666350@gmail.com>
2020-06-02 02:59:26 +0300

    fix portfolio view path

--- src/main/java/by/jrr/constant/View.java
+++ src/main/java/by/jrr/constant/View.java
@@ -16,10 +16,11 @@ public class View {
     public static final String PROJECT_LIST = "project/projectList";
     public static final String ISSUE = "project/issue";
     public static final String ISSUE_LIST = "project/issueList";
-    public static final String DOMAIN = "domain/domain";
-    public static final String DOMAIN_LIST = "domain/projectList";
-    public static final String SUBJECT = "domain/subject";
-    public static final String SUBJECT_LIST = "domain/subjectList";
+
+    public static final String DOMAIN = "portfolio/domain";
+    public static final String DOMAIN_LIST = "portfolio/domainList";
+    public static final String SUBJECT = "portfolio/subject";
+    public static final String SUBJECT_LIST = "portfolio/subjectList";
 
     public static final String Q_AND_A = "interview/question";
     public static final String Q_AND_A_LIST = "interview/questionList";
