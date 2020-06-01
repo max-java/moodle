60631eebc70480e61c7519dcf120705cda6c1101
max-java <6666350@gmail.com>
2020-06-02 02:59:00 +0300

    add Portfolio model

---
+++ src/main/java/by/jrr/portfolio/bean/SubjectType.java
@@ -0,0 +1,8 @@
+package by.jrr.portfolio.bean;
+/**
+ * This is IssueType class analog for user outer pages and articles
+ * */
+
+public enum SubjectType {
+    NONE, ARTICLE, PROFILE, CURRICULUM_VITAE
+}
