26d2b95a2e687b785ab8f95d808a1e5b8273630b
max-java <6666350@gmail.com>
2020-06-05 03:33:26 +0300

    fix minor changes in downloading files

--- src/main/java/by/jrr/files/controller/FileDownloadingController.java
+++ src/main/java/by/jrr/files/controller/FileDownloadingController.java
@@ -3,6 +3,7 @@ package by.jrr.files.controller;
 import by.jrr.constant.Endpoint;
 import by.jrr.files.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class FileDownloadingController {
     public byte[] getFileBytesByFilename(@PathVariable String fileName)  {
         return fileService.getFileBytes(fileName);
     }
-    @RequestMapping(value = Endpoint.PDF+"/{fileName}")
+    @RequestMapping(value = Endpoint.PDF+"/{fileName}", produces = MediaType.APPLICATION_PDF_VALUE)
     @ResponseBody
     public byte[] getPdfBytesByFilename(@PathVariable String fileName)  {
         return fileService.getFileBytes(fileName);
