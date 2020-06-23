3cfd78b35f2bb05f98cda867a84b388801793623
max-java <6666350@gmail.com>
2020-06-23 15:37:50 +0300

    Fix error message for non unique email.

--- src/main/java/by/jrr/api/controller/RegisterAndSubscribeFormValidationController.java
+++ src/main/java/by/jrr/api/controller/RegisterAndSubscribeFormValidationController.java
@@ -2,10 +2,13 @@ package by.jrr.api.controller;
 
 import by.jrr.auth.service.UserValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.NonUniqueResultException;
 
 
 @RestController
@@ -19,6 +22,11 @@ public class RegisterAndSubscribeFormValidationController {
         RestResponse restResponse = new RestResponse(true);
         try {
             userValidationService.validateEmail(email);
+        } catch (NonUniqueResultException | IncorrectResultSizeDataAccessException e) {
+            restResponse.setValidationPassed(false);
+            restResponse.setError("Пользователь с e-mail "+ email+ " уже зарегистрирован. Попробуйте другой.");
+            // TODO: 23/06/20 impossible situation, login-email duplicates in DB, add logging
+            // TODO: 23/06/20 It is possible for user, that creates stream | team. Consider to handle that situations
         } catch (Exception ex) {
             restResponse.setValidationPassed(false);
             restResponse.setError(ex.getMessage());
