0a701d6cee4ca9c0669f64b4b8998479d2cca2f2
max-java <6666350@gmail.com>
2020-07-05 04:23:43 +0300

    add student activity online checking in admin profile card

--- src/main/java/by/jrr/registration/service/StudentActionToLogService.java
+++ src/main/java/by/jrr/registration/service/StudentActionToLogService.java
@@ -1,13 +1,19 @@
 package by.jrr.registration.service;
 
+import by.jrr.profile.bean.Profile;
 import by.jrr.profile.service.ProfileService;
+import by.jrr.profile.service.StreamAndTeamSubscriberService;
 import by.jrr.registration.bean.EventType;
 import by.jrr.registration.bean.StudentActionToLog;
+import by.jrr.registration.controller.LogActionAndRedirectController;
 import by.jrr.registration.repository.StudentActionToLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentActionToLogService {
@@ -16,6 +22,8 @@ public class StudentActionToLogService {
     StudentActionToLogRepository satlR;
     @Autowired
     ProfileService profileService;
+    @Autowired
+    StreamAndTeamSubscriberService streamAndTeamSubscriberService;
 
     public void saveAction(Long streamTeamId, String eventType, String link) {
 
@@ -28,5 +36,53 @@ public class StudentActionToLogService {
                         .urlToRedirect(link)
                         .build()
         );
+    }
+
+    public LogActionAndRedirectController.UserActivityDTO findActionForStreamBetweenTimestamps(Long streamProfileId, LocalDateTime start, LocalDateTime end) {
+        LogActionAndRedirectController.UserActivityDTO userActivityDTO = new LogActionAndRedirectController.UserActivityDTO();
+        List<StudentActionToLog> studentActionToLogList = satlR.findAllByStreamTeamProfileIdAndTimestampBetween(streamProfileId, start,end);
+        List<LogActionAndRedirectController.UserActivityElementDTO> active = studentActionToLogList.stream()
+                .map(a -> a.getStudentProfileId())
+                .map(id -> profileService.findProfileByProfileId(id).orElseGet(Profile::new))
+                .map(profile -> setDTO(profile))
+                .collect(Collectors.toList());
+
+        List<LogActionAndRedirectController.UserActivityElementDTO> subscribers = streamAndTeamSubscriberService.getAllSubscribersForProfileByProfileId(streamProfileId).stream()
+                .map(sub -> sub.getSubscriberProfileId())
+                .map(id -> profileService.findProfileByProfileId(id).orElseGet(Profile::new))
+                .map(profile -> setDTO(profile))
+                .collect(Collectors.toList());
+
+        List<LogActionAndRedirectController.UserActivityElementDTO> notActiveOfSubscribers = new ArrayList<>();
+        for (LogActionAndRedirectController.UserActivityElementDTO sub : subscribers) {
+            if (active.stream().noneMatch(a -> a.getUserName().equals(sub.getUserName()))) {
+                notActiveOfSubscribers.add(sub);
+            }
+        }
+
+        userActivityDTO.setActive(active);
+        userActivityDTO.setNotActive(notActiveOfSubscribers);
+        return userActivityDTO;
+    }
+    private LogActionAndRedirectController.UserActivityElementDTO setDTO(Profile profile) {
+        LogActionAndRedirectController.UserActivityElementDTO elementDTO = new LogActionAndRedirectController.UserActivityElementDTO();
+        if(profile != null && profile.getUser() != null) {
+            elementDTO.setFirstName(profile.getUser().getName());
+            elementDTO.setLastName(profile.getUser().getLastName());
+            elementDTO.setEmail(profile.getUser().getEmail());
+            elementDTO.setPhone(profile.getUser().getPhone());
+            elementDTO.setUserName(profile.getUser().getUserName());
+        } else {
+            elementDTO.setFirstName(".");
+            elementDTO.setLastName(".");
+            elementDTO.setEmail(".");
+            elementDTO.setPhone(".");
+            elementDTO.setUserName(".");
+        }
+        //костыль для тех, кто зарегался, когда фелефон был необязательным полем (для того, что бы на хендлить каждое значение на стороне JS)
+        if (elementDTO.getPhone() == null) {
+            elementDTO.setPhone(".");
+        }
+        return elementDTO;
     }
 }
