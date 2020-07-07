e4585f4481ef07c38320c4e8067dccc0fb6b9615
max-java <6666350@gmail.com>
2020-07-07 18:30:15 +0300

    Add approve team subscription feature

--- src/main/java/by/jrr/profile/service/StreamAndTeamSubscriberService.java
+++ src/main/java/by/jrr/profile/service/StreamAndTeamSubscriberService.java
@@ -1,6 +1,8 @@
 package by.jrr.profile.service;
 
+import by.jrr.auth.bean.Role;
 import by.jrr.auth.bean.UserRoles;
+import by.jrr.auth.service.UserAccessService;
 import by.jrr.auth.service.UserService;
 import by.jrr.constant.LinkGenerator;
 import by.jrr.email.service.EMailService;
@@ -28,6 +30,8 @@ public class StreamAndTeamSubscriberService {
     @Autowired
     UserService userService;
     @Autowired
+    UserAccessService userAccessService;
+    @Autowired
     EMailService eMailService;
 
 
@@ -39,6 +43,12 @@ public class StreamAndTeamSubscriberService {
         streamAndTeamSubscriberList.forEach(s -> s.setSubscriptionProfile(profileService.findProfileByProfileIdLazy(s.getStreamTeamProfileId()).get())); // TODO: 24/06/20  get rid of strange .get(), maybe use orElseGet, but what else to get?
         return streamAndTeamSubscriberRepository.findAllBySubscriberProfileId(id);
     }
+    public boolean isUserSubscribedForProfile(Long streamAndTeamProfileId,
+                                              Long subscriberProfileId) {
+        Optional<StreamAndTeamSubscriber> subscriberOptional = streamAndTeamSubscriberRepository
+                .findAllByStreamTeamProfileIdAndSubscriberProfileId(streamAndTeamProfileId, subscriberProfileId);
+        return subscriberOptional.isPresent();
+    }
 
     public StreamAndTeamSubscriber updateSubscription(Long streamAndTeamProfileId,
                                                       Long subscriberProfileId,
@@ -49,6 +59,14 @@ public class StreamAndTeamSubscriberService {
             return updateSubscriptionWithNewStatus(subscriberOptional, status);
         } else {
             return createSubscriptionAndSetStatus(streamAndTeamProfileId, subscriberProfileId, status);
+        }
+    }
+    public void deleteSubscription(Long streamAndTeamProfileId,
+                                   Long subscriberProfileId) {
+        Optional<StreamAndTeamSubscriber> subscriberOptional = streamAndTeamSubscriberRepository
+                .findAllByStreamTeamProfileIdAndSubscriberProfileId(streamAndTeamProfileId, subscriberProfileId);
+        if (subscriberOptional.isPresent()) {
+            streamAndTeamSubscriberRepository.delete(subscriberOptional.get());
         }
     }
 
@@ -72,11 +90,20 @@ public class StreamAndTeamSubscriberService {
         //change role
         try {
             Profile subscriberProfile = profileService.findProfileByProfileId(subscriberOptional.get().getSubscriberProfileId()).get(); // TODO: 23/06/20 I should have entity with all fields populated in this place
-            userService.addRoleToUser(UserRoles.ROLE_FREE_STUDENT, subscriberProfile.getUserId()); // TODO: 25/06/20 should role be changed here like this?
+            if(!userAccessService.isUserhasRole(UserRoles.ROLE_FREE_STUDENT)){
+                userService.addRoleToUser(UserRoles.ROLE_FREE_STUDENT, subscriberProfile.getUserId()); // TODO: 25/06/20 should role be changed here like this?
+            }
         } catch (Exception ex) {
             System.out.println(" [ error on attempt to extract userId from subscriber]");
         }
         //send confirmation email
+        new Thread(() -> sendConfirmationEmail(subscriberOptional, status)).start();
+        //change subscription status
+        subscriberOptional.get().setStatus(status);
+        return streamAndTeamSubscriberRepository.save(subscriberOptional.get());
+    }
+
+    private void sendConfirmationEmail(Optional<StreamAndTeamSubscriber> subscriberOptional, SubscriptionStatus status) {
         // TODO: 23/06/20 add variables to email template and chose email based on subscription status and profile type. (stream or team)
         if (subscriberOptional.isPresent()) {
             if (status.equals(SubscriptionStatus.APPROVED)) {
@@ -90,15 +117,17 @@ public class StreamAndTeamSubscriberService {
                     String telegramLink = streamTeamProfile.getTelegramLink();
                     String to = subscriberProfile.getUser().getEmail();
 
-                    eMailService.sendStreamSubscriptionConfirmation(firstAndLastName, teamStreamName, streamTeamLink, telegramLink, to);
+                    if(streamTeamProfile.getUser().getRoles().contains(UserRoles.ROLE_STREAM)) { // TODO: 07/07/20 move all user methods like this to profile
+                        eMailService.sendStreamSubscriptionConfirmation(firstAndLastName, teamStreamName, streamTeamLink, telegramLink, to);
+                    }
+                    if(streamTeamProfile.getUser().getRoles().contains(UserRoles.ROLE_TEAM)){
+                        eMailService.sendTeamSubscriptionConfirmation(firstAndLastName, teamStreamName, streamTeamLink, to);
+                    }
                 } catch (Exception ex) {
                     System.out.println(" [ error on attempt to extract data to send email confirmation from subscriber]");
                 }
             }
         }
-        //change subscription status
-        subscriberOptional.get().setStatus(status);
-        return streamAndTeamSubscriberRepository.save(subscriberOptional.get());
     }
 
 }
