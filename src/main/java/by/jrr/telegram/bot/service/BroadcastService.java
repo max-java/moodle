aea1709f12009debb153a4a87193fa9172fedf6c
max-java <6666350@gmail.com>
2020-07-14 15:39:20 +0300

    add sout to Broadcast service to make sure that it cycling correctly

--- src/main/java/by/jrr/telegram/bot/service/BroadcastService.java
+++ src/main/java/by/jrr/telegram/bot/service/BroadcastService.java
@@ -21,8 +21,10 @@ public class BroadcastService {
 
     public void broadcast() {
         while (true) {
+            System.out.println("\"while true cycling\" = " + "while true cycling"); // TODO: 14/07/20 replace with logs
             LocalTime now = LocalTime.now();
             if (now.isAfter(LocalTime.of(10, 00)) && now.isBefore(LocalTime.of(20, 00))) {
+                System.out.println("broadcast - it is right time to send message");
                 QAndA qAndA = qAndAService.getRandomQuestion();
                 String text = qAndA.getQuestion();
                 messageService.sendMessage(Chat.JG_ALUMNI, text);
@@ -30,6 +32,7 @@ public class BroadcastService {
                 messageService.sendMessage(Chat.JG_ALUMNI, text);
             }
             try {
+                System.out.println("\"broadcast is sleeping\" = " + "broadcast is sleeping");
                 Thread.sleep(28800000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -41,6 +44,7 @@ public class BroadcastService {
     public void startBroadcasting() {
         if (!isBroadcasting) {
             isBroadcasting = true;
+            System.out.println("\"start broadcasting\" = " + "start broadcasting");
             broadcast();
         }
     }
