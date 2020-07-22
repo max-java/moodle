f604048f946ad0fa07929801717138097950ce58
max-java <6666350@gmail.com>
2020-07-22 13:39:20 +0300

    Add endpoint for admin to register users

--- src/main/java/by/jrr/constant/Endpoint.java
+++ src/main/java/by/jrr/constant/Endpoint.java
@@ -48,6 +48,8 @@ public class Endpoint {
 
     public static final String REGISTER_USER_AND_ENROLL_TO_STREAM = "/registerAndSubscribe";
     public static final String REGISTER_USER = "/registration";
+    public static final String REGISTER_USER_ADMIN_REST = "/admin/api/users/";
+    public static final String REGISTER_USER_ADMIN_REST_ERRORS = "errors";
 
     public static final String BOOK_LIST = "/books";
     public static final String BOOK = "/book";
