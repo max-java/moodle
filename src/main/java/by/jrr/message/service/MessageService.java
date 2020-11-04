1ed737c68a0378e1eccfacad028230793c702a4f
max-java <6666350@gmail.com>
2020-11-04 03:55:03 +0300

    update message DTO, and minor chages

--- src/main/java/by/jrr/message/service/MessageService.java
+++ src/main/java/by/jrr/message/service/MessageService.java
@@ -7,6 +7,8 @@ import by.jrr.api.model.UserContactsDto;
 import by.jrr.api.proxy.MessageProxy;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,10 @@ public class MessageService {
     @Autowired
     MessageProxy messageProxy;
 
+    Logger log = LoggerFactory.getLogger(MessageService.class); // TODO: 04/11/2020 moveto bean
+
     public void sendMessageDtoWitContactData(UserContactsDto userContactsDto, Long profileId) {
+        log.info("trying to send userContactsDto: {}", userContactsDto.toString());
         try {
             MessageDto mess = MessageDto.builder()
                     .messageText(objectMapper.writeValueAsString(userContactsDto))
@@ -28,9 +33,14 @@ public class MessageService {
             mess.setType(MessageType.CONTACT_DATA);
             mess.setUserProfileId(profileId);
             messageProxy.postNewMessage(mess);
+            log.info("MessageDtoWitContactData has been sent: {}", mess.toString());
         } catch (JsonProcessingException e) {
+            log.info(JsonProcessingException.class.toString());
+            e.printStackTrace();
             // TODO: 02/11/2020 consider to log and create backup strategy
         } catch (Exception e) {
+            log.info(Exception.class.toString());
+            e.printStackTrace();
             // TODO: 02/11/2020 consider to log and create backup strategy
         }
     }
