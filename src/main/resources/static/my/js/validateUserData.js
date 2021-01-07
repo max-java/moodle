86bf1fd428dde5c3e538cd6432fc9128d26ec86e
Maksim Shelkovich <6666350@gmail.com>
2021-01-07 07:25:04 +0300

    2021-01-07: fixes id duplicated on userData forms (#239)
    * 201217 billing feature (#227)
    
    * 201217: adds total for user salary if paid
    
    * 201217: adds billing to user page for user view
    
    * 201217: adds total debt to navbar
    
    * fix double user info on profile
    
    * removes redirection to tg on register and subscribe
    
    * 201217 feedback (#230)
    
    * 201217: adds total for user salary if paid
    
    * 201217: adds billing to user page for user view
    
    * 201217: adds total debt to navbar
    
    * fix double user info on profile
    
    * removes redirection to tg on register and subscribe
    
    * adds createReviewRequest in single method
    
    * 20201226 feedback feature (#231)
    
    * 20201226: adds RequestForReviewDto and simplifies request for review creation process
    
    * in createRequestForReview parameters replaced by Map
    
    * 20201226: creates review request button
    
    * 20201226: removes old button
    
    * 20201226: makes requestForReview editable and deletable
    
    * 20201226: adds requests for same issue feature and dates on reviews
    
    * 20201226: makes reviewRequests reverse sorted, some ui styling
    
    * 20201226: review button styling
    
    * 20201226: review button styling
    
    * 20201227: adds filter for reviewRequests (#235)
    
    * 2021-01-07: adds new user data validation endpoint
    
    * 2021-01-07: fixes id duplicated on userData forms

--- src/main/resources/static/my/js/validateUserData.js
+++ src/main/resources/static/my/js/validateUserData.js
@@ -26,7 +26,7 @@ class UserValidationResponse {
     errors;
 }
 
-function validateForm() {
+function validateForm(suffix) {
     var xhr = new XMLHttpRequest();
     var url = "/auth/validate/user";
     xhr.open("POST", url, true);
@@ -35,42 +35,44 @@ function validateForm() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
             if(response.userValidationStatus === "ERROR") {
-                setErrorFields(response);
+                setErrorFields(response, suffix);
             } else {
-                clearErrorFields();
+                clearErrorFields(suffix);
             }
         }
     };
 
     var request = new UserValidationRequest(
-        document.getElementById("firstAndLastName").value,
-        document.getElementById("email").value,
-        document.getElementById("phone").value
+        document.getElementById("firstAndLastName"+suffix).value,
+        document.getElementById("email"+suffix).value,
+        document.getElementById("phone"+suffix).value
     );
     xhr.send(JSON.stringify(request));
 }
 
-function setErrorFields(response) {
-    clearErrorFields();
-    if (response.errors.FIRST_AND_LAST_NAME) {
-        document.getElementById("FIRST_AND_LAST_NAME_ERROR").innerHTML = response.errors.FIRST_AND_LAST_NAME;
-    } else {
-        document.getElementById("FIRST_AND_LAST_NAME_ERROR").innerHTML = "";
-    }
-    if (response.errors.EMAIL) {
-        document.getElementById("EMAIL_ERROR").innerHTML = response.errors.EMAIL;
-    } else {
-        document.getElementById("EMAIL_ERROR").innerHTML = "";
-    }
-    if (response.errors.PHONE) {
-        document.getElementById("PHONE_ERROR").innerHTML = response.errors.PHONE;
-    } else {
-        document.getElementById("PHONE_ERROR").innerHTML = "";
+function setErrorFields(response, suffix) {
+    clearErrorFields(suffix);
+    if(response.errors) {
+        if (response.errors.FIRST_AND_LAST_NAME) {
+            document.getElementById("FIRST_AND_LAST_NAME_ERROR" + suffix).innerHTML = response.errors.FIRST_AND_LAST_NAME;
+        } else {
+            document.getElementById("FIRST_AND_LAST_NAME_ERROR" + suffix).innerHTML = "";
+        }
+        if (response.errors.EMAIL) {
+            document.getElementById("EMAIL_ERROR" + suffix).innerHTML = response.errors.EMAIL;
+        } else {
+            document.getElementById("EMAIL_ERROR" + suffix).innerHTML = "";
+        }
+        if (response.errors.PHONE) {
+            document.getElementById("PHONE_ERROR" + suffix).innerHTML = response.errors.PHONE;
+        } else {
+            document.getElementById("PHONE_ERROR" + suffix).innerHTML = "";
+        }
     }
 }
 
-function clearErrorFields() {
-    document.getElementById("FIRST_AND_LAST_NAME_ERROR").innerHTML = "";
-    document.getElementById("EMAIL_ERROR").innerHTML = "";
-    document.getElementById("PHONE_ERROR").innerHTML = "";
+function clearErrorFields(suffix) {
+    document.getElementById("FIRST_AND_LAST_NAME_ERROR"+suffix).innerHTML = "";
+    document.getElementById("EMAIL_ERROR"+suffix).innerHTML = "";
+    document.getElementById("PHONE_ERROR"+suffix).innerHTML = "";
 }
